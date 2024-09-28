0e261efd47fcb77e05ffae34cff2a8ad5bbf0c69
Yellowstrawberrys <dirjhhsg@gmail.com>
2024-09-28 21:54:18 +0900

    test commit

---
+++ src/main/java/net/yellowstrawberry/solveana/SolveAnAApplication.java
@@ -0,0 +1,13 @@
+package net.yellowstrawberry.solveana;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SolveAnAApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SolveAnAApplication.class, args);
+    }
+
+}
---
+++ src/test/java/net/yellowstrawberry/solveana/SolveAnAApplicationTests.java
@@ -0,0 +1,13 @@
+package net.yellowstrawberry.solveana;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SolveAnAApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
